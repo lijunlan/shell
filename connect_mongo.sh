6ca3308021c84f97e98cea6fe624fe7ff46110dc
junlanli <ll1992917>
2016-10-21 14:42:28 +0800

    add generator mongo pojo tag script

--- connect_mongo.sh
+++ connect_mongo.sh
@@ -4,4 +4,15 @@
 
 #mongo 115.159.87.160/luna-dev -u luna-dev -p dsidiejvng
 #mongo 47.90.63.120/admin -u root -p ll1992917
-mongo 47.90.63.120/mydb -u mydb -p 123456
+#mongo 47.90.63.120/mydb -u mydb -p 123456
+if [ $# = 1 ] ; then
+        if [ $1 = "focus_audit" ] ; then
+        	mongo 10.10.196.6/admin -u focus_audit -p qwe123!@#
+	elif [ $1 = "mymongo" ] ; then
+        	mongo 47.90.63.120/mydb -u mydb -p 123456
+	else
+                 echo "unknow parameter,please input newhouse_test/focus_dc"
+        fi
+else
+         echo "please input parameter: focus_audit/mymongo"
+fi
---
+++ generator_mongo_pojo_tag.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+awk  '/JSONField\(name/{print $0}' $1 | sed 's/\(@JSONField(name.*\)"\(.*\)"\(.*\)/\2;\2/g' | awk -F ';' '{print toupper($1)";"$2}' | sed 's/^ */String JSON_FIELD_/g' | sed 's/;/ = "/g' | sed 's/$/";/g' | awk '{print $0;print ""}'>> temp
+
+awk  '/Field\(value/{print $0}' $1 | sed 's/\(@Field(value.*\)"\(.*\)"\(.*\)/\2;\2/g' | awk -F ';' '{print toupper($1)";"$2}' | sed 's/^ */String FIELD_/g' | sed 's/;/ = "/g' | sed 's/$/";/g' | awk '{print $0;print ""}' >> temp
+
+echo 'TAGS:\n'
+
+cat temp
+
+echo '\n[INFO]  please add FIELD_XXX_ID = _id by yourself'
+
+rm temp
